a53e59b43bf1a6fec9519081c2ca94cc0f9e570e
Sri <sri@Sris-MacBook-Pro.local>
2025-04-30 01:35:01 +0530

    db to prod

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime
 from sqlalchemy.orm import declarative_base, sessionmaker
 from datetime import datetime
 
-DATABASE_URL = "sqlite:///./sales.db"
+DATABASE_URL = "postgresql://sales_backend_user:ootonymGVB9WBTnjwpJPYgQVFn0ZT2z0@dpg-d08j0geuk2gs73bc00ig-a/sales_backend"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
